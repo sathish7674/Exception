e53e0b86e9c86cdf0cccb7ace9f37bef88288aef
sathish7674 <orsusathish1997@gmail.com>
2022-07-08 10:11:42 +0530

    modefied file

--- Exception2.java
+++ Exception2.java
@@ -3,6 +3,14 @@ package Exceotion;
 public class Exception2 {
 public static  void main(String args[])
 {
+	public void m1
+	{
+
+	}
+	public void m2
+	{
+		
+	}
 	try {
 		int i=100/0;
 	}catch (Exception e)
